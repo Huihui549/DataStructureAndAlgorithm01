090785c2c888cc8a1bff99ba6d20e8cabd479036
mgh <1755494398@qq.com>
2023-05-11 16:40:06 +0800

    hot-fix commit

--- src/Algorithm/Sort/BubbleSort.java
+++ src/Algorithm/Sort/BubbleSort.java
@@ -24,8 +24,8 @@ public class BubbleSort {
         //创建并初始化一个数组
 //        int[] nums = {3,9,-1,10,-2,20,25};
 //
-///
-        //
+//hot-fix
+
         //创建一个80000个随机数的数组
         int[] nums = new int[80000];
         for (int i = 0; i < nums.length; i++) {
